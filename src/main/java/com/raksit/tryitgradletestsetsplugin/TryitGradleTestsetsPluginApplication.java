2aab957ebfcd447e69a6e018e85e7c815db6e233
Raksit <raksit.m@ku.th>
2020-01-19 12:33:44 +0700

    :tada:

---
+++ src/main/java/com/raksit/tryitgradletestsetsplugin/TryitGradleTestsetsPluginApplication.java
@@ -0,0 +1,13 @@
+package com.raksit.tryitgradletestsetsplugin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryitGradleTestsetsPluginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TryitGradleTestsetsPluginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raksit/tryitgradletestsetsplugin/TryitGradleTestsetsPluginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raksit.tryitgradletestsetsplugin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TryitGradleTestsetsPluginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
