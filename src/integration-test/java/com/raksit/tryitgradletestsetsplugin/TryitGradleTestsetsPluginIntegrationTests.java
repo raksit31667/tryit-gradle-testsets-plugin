c334c96c533a47618c026b77170103ed47cf89f5
Raksit <raksit.m@ku.th>
2020-01-19 15:51:13 +0700

    :white_check_mark: installed some specified dependency for integration testing as an example

--- src/integration-test/java/com/raksit/tryitgradletestsetsplugin/TryitGradleTestsetsPluginIntegrationTests.java
+++ src/integration-test/java/com/raksit/tryitgradletestsetsplugin/TryitGradleTestsetsPluginIntegrationTests.java
@@ -2,12 +2,18 @@ package com.raksit.tryitgradletestsetsplugin;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 @SpringBootTest
 class TryitGradleTestsetsPluginIntegrationTests {
 
 	@Test
 	void someIntegrationTest() {
+		try (PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>()) {
+			postgreSQLContainer.start();
+
+			// do something with database
+		}
 	}
 
 }
